72899fd76346f1be6349de829aec3485552cc947
Simas <baka367@gmail.com>
2022-10-15 14:42:49 +0800

    delete target dir

--- user/target/classes/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO addresses (id, city, country) VALUES
-   (1, 'Washington', 'US'),
-   (2, 'Ice Kingdom', 'Fairyland'),
-   (3, '213ADD2F', 'Mars');
-
-INSERT INTO users (id, name, username, address_id) VALUES
-  (1, 'John Wick', 'johnybravo87', 1),
-  (2, 'Elza', 'tooc0ldforu', 2),
-  (3, 'El Kunga', 'sleepyhead', 3);
